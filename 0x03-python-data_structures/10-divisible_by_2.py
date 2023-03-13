3397a2b91ebae478b4978fdb614f4b0e11e90b03
ChyOakley <chiomaameh1@gmail.com>
2023-03-13 09:15:19 -0700

    Tasks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+
 def divisible_by_2(my_list=[]):
-    new_l = my_list.copy()
-    for i in range(0, len(my_list)):
+    multiples = []
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            new_l[i] = 1
+            multiples.append(True)
         else:
-            new_l[i] = 0
-    return 
+            multiples.append(False)
+
+    return (multiples)
