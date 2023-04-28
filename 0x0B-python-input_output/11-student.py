df1962b7768d9f18ba37aae4376c80e9915af48e
ChyOakley <chiomaameh1@gmail.com>
2023-04-28 09:34:52 -0700

    Tasks

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python3
-"""Defines a Pascal's Triangle function."""
+
+"""Defines a Student class"""
 
 
-def pascal_triangle(n):
-    """Represent Pascal's Triangle of size n.
-    Returns a list of lists of integers representing the triangle.
-    """
-    if n <= 0:
-        return []
+class Student:
+    """A Student class"""
 
-    triangles = [[1]]
-    while len(triangles) != n:
-        tri = triangles[-1]
-        tmp = [1]
-        for i in range(len(tri) - 1):
-            tmp.append(tri[i] + tri[i + 1])
-        tmp.append(1)
-        triangles.append(tmp)
-    return triangles
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student object
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student
+        If attrs is a list of strings, only attribute names
+        contained in this list are retrieved.
+        Args:
+            attrs (list): attributes to retrieve
+        """
+        if type(attrs) == list:
+            if all(type(elem) == str for elem in attrs):
+                return {key: getattr(self, key)
+                        for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of Student instance
+        Args:
+            json (dict): key-value pair to replace attributes with
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
