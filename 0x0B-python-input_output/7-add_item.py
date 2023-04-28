c3c5599ec011af2ad3e408ce5ad78383e5b1de45
ChyOakley <chiomaameh1@gmail.com>
2023-04-28 09:08:55 -0700

    Tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,24 +3,16 @@
 
 import sys
 
-
-def chioma():
     """a script that adds arguments to a python file"""
 
-    save_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-    file = "add_item.json"
-    json_list = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        load_json_file(file)
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        json_list = []
-
-    json_list.extend(sys.argv[1:])
-    save_file = save_json_file(json_list, file)
-    return save_file
-
-
-chioma()
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
